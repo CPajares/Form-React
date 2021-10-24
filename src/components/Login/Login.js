505bae4b1b65c3769b86be48db1f819cc83efbcc
Carlos Pajares <c.pajares@hotmail.com>
2021-10-24 13:58:22 +0200

    created Showdata component to includ in login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import { useContext } from "react";
 import FormContext from "../../context/Form";
 import ButtonB from "../Button/Button";
+import ShowData from "../ShowData/Showdata";
 
 const Login = () => {
-  const { previous } = useContext(FormContext);
+  const { onChangeprueba, data, previous } = useContext(FormContext);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("yes");
+    if (
+      data.username === data.usernameCheck &&
+      data.password === data.passwordCheck
+    ) {
+      setshowAllDetails(true);
+    } else {
+      alert("por favor... esa memoria, no coinciden sus datos");
+    }
   };
+
+  const [showAllDetails, setshowAllDetails] = useState(false);
 
   const previousDefault = () => {
     previous();
@@ -17,24 +28,26 @@ const Login = () => {
     <>
       <form onSubmit={handleSubmit}>
         <div className="form-group">
-          3<label htmlFor="userName">User Name</label>
+          3<label htmlFor="usernameCheck">User Name</label>
           <input
             type="text"
             className="form-control"
-            id="userName"
+            id="usernameCheck"
             placeholder="Example input"
             autoComplete="off"
+            onChange={onChangeprueba}
           />
         </div>
         <div className="form-group">
-          <label htmlFor="password">Password</label>
+          <label htmlFor="passwordCheck">Password</label>
           <input
             type="password"
             className="form-control"
-            id="password"
+            id="passwordCheck"
             placeholder="Another input"
             required
             autoComplete="off"
+            onChange={onChangeprueba}
           />
         </div>
         <div className="form-check">
@@ -59,6 +72,10 @@ const Login = () => {
           className={"btn btn-info"}
         />
       </form>
+      {showAllDetails ? <ShowData /> : ""}
+      <pre style={{ textAlign: "left" }}>
+        <div>{JSON.stringify(data)}</div>
+      </pre>
     </>
   );
 };
